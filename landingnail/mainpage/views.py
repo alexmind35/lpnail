bd54bcc3e7d30a504a964f085e90411cac92011d
Alexander Mikhaylov <alexmind@myandex.ru>
2018-12-01 19:38:43 +0300

    work apps Logotype + Mainpage

--- landingnail/mainpage/views.py
+++ landingnail/mainpage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from logotype.models import Logo
+from mainpage.models import About
 
-# Create your views here.
+def mainpage_list(request):
+    logotype = Logo.objects.all()
+    mainpage = About.objects.all()
+    return render(request, "mainpage/mainpage_list.html", {"mainpage": mainpage, "logotype": logotype})
